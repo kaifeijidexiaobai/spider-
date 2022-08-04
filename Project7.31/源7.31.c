dec94b73479a23c6c1f3bc1658077095c93943b5
kaifeijidexiaobai <110513536+kaifeijidexiaobai@users.noreply.github.com>
2022-08-04 10:08:40 +0800

    Add files via upload

---
+++ Project7.31/源7.31.c
@@ -0,0 +1,43 @@
+#define  _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+////int main()
+// {
+////    char arr1[] = "abc";
+////    char arr2[] = {'a' ,'b','c',};
+////    printf("%d\n", strlen(arr1));
+////    printf("%d\n", strlen(arr2));
+////    return 0;  
+////
+////}
+////    
+////{
+////	printf("c:\\test\\32\\test.c");
+////
+////	return 0;
+////}
+
+
+////int main()
+// {
+////	printf("%d\n", strlen("c:\test\32\test.c"));
+////	return 0;
+////}
+int main()
+{
+	int input = 0;
+	printf("进大学\n");
+	printf("好好学习?(1/0)>:");
+	scanf("%d", &input);
+	if (input == 1)
+	printf("好offer");
+	else
+		printf("卖红薯");
+	return 0;
+}
+
+
+
+
+
+
